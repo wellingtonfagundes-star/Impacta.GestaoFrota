6927652d2312b76f1be8edf76441cbf9c03ac478
Wellington Fagundes <wellington.fagundes@aluno.impacta.edu.br>
2026-09-17 22:30:05 -0300

    Inicio Implementação Controller de Veículos - "Projeto GestaoFrotasWEB" - (By Project Academy Fac. Impacta) - Part. 1

---
+++ 1 - UI/Impacta.GestaoFrota.UI/Controllers/VeiculoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Impacta.GestaoFrota.UI.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
